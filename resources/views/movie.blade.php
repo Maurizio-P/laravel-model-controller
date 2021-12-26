46250335ba900a2695ae6e745bf4ea420ec3e210
Maurizio-P <mauriziopignotti@gmail.com>
2021-12-26 12:22:47 +0100

    complete

---
+++ resources/views/movie.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movie</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <h1>Movie</h1>
+    <div class="row">
+
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <h3>
+                        {{$movie['title']}}
+                    </h3>
+                    <h6>{{$movie['vote']}}</h6>
+                </div>
+            @endforeach
+    </div>
+</body>
+</html>
